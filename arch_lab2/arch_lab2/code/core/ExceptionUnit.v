8b81e20fea0d5597888923cdd3a2cc66ae0493e0
Li_Yvanrun <3230105347@zju.edu.cn>
2025-03-20 20:22:36 +0800

    补充了Exception模块

--- arch_lab2/arch_lab2/code/core/ExceptionUnit.v
+++ arch_lab2/arch_lab2/code/core/ExceptionUnit.v
@@ -18,25 +18,115 @@ module ExceptionUnit(
 
     input mret,//返回信号，从cs指令中返回
 
-    input[31:0] epc_cur,
+    input[31:0] epc_cur,//接的是PC_WB，PC_WB来自前面的阶段对每个阶段进行判断，如果没有flush 就赋0；如果有flush，就取上一个阶段的PC。如果一直flush可以追溯到PCurrent_ID
+    //这个阶段如果Data_stall/flush，就不进行异常处理，直接跳转到下一个阶段。
+    //如果没有Data_stall/flush，就进行异常处理，PCurrent_ID<=PCOUT(就是PC_IF)
     input[31:0] epc_next,
-    output[31:0] PC_redirect,
-    output redirect_mux,
+    //接的是~isFlushed_MEM ? PC_MEM : ~isFlushed_EXE ? PC_EXE :~isFlushed_ID ? PC_ID : PC_IF 意思是哪个阶段如果发生异常，就跳转到哪个PC。如果都没有flush，就从PC_IF开始执行。
+    output[31:0] PC_redirect,//接的是PC_redirect_exp 意思是如果发生异常，就把PC_redirect_exp的值写到PC_WB中，然后跳转到异常处理程序。
+    output redirect_mux,//接的是redirect_mux_exp，最后传到CPUTEST的exp_sig中 意思是如果发生异常，就把redirect_mux_exp的值写到redirect_mux中，然后根据redirect_mux的值跳转到对应的异常处理程序。
 
-    output reg_FD_flush, reg_DE_flush, reg_EM_flush, reg_MW_flush, 
-    output RegWrite_cancel
+    output reg_FD_flush, reg_DE_flush, reg_EM_flush, reg_MW_flush, //各个寄存器的flush
+    output RegWrite_cancel //取消向寄存器中写
 );
 
     reg[11:0] csr_raddr, csr_waddr;
     reg[31:0] csr_wdata;
     reg csr_w;
     reg[1:0] csr_wsc;
+    reg[1:0] cur_state,next_state;
 
-    wire[31:0] mstatus;
-
+    // CSR接口信号
+    wire [31:0] mstatus;
+    wire [31:0] mepc;
+    // CSR寄存器地址定义
+    parameter MSTATUS = 12'h300;
+    parameter MTVEC   = 12'h305;
+    parameter MEPC    = 12'h341;
+    parameter MCAUSE  = 12'h342;
+    parameter MTVAL   = 12'h343;
+   
     CSRRegs csr(.clk(clk),.rst(rst),.csr_w(csr_w),.raddr(csr_raddr),.waddr(csr_waddr),
         .wdata(csr_wdata),.rdata(csr_r_data_out),.mstatus(mstatus),.csr_wsc_mode(csr_wsc));
 
     //According to the diagram, design the Exception Unit
+    localparam STATE_IDLE = 2'b00; //检测异常，存储附加异常信息，更新机器状态
+    localparam STATE_MEPC = 2'b01; //更新mepc寄存器
+    localparam STATE_MCAUSE = 2'b10; //更新mcause寄存器
+    // 异常类型编码
+    wire [31:0] exception_cause;
+    assign exception_cause = 
+        illegal_inst  ? 32'h2 :
+        ecall_m       ? 32'hB :
+        l_access_fault? 32'h5 :
+        s_access_fault? 32'h7 :
+        interrupt     ? {1'b1, 31'hB} : // 假设外部中断
+        32'h0;
+
+   always @(posedge clk or posedge rst) begin
+        if (rst) cur_state <= STATE_IDLE;
+        else cur_state <= next_state;
+    end
+
+    always @(*) begin
+        case (cur_state)
+            STATE_IDLE:begin
+                if (|{illegal_inst, ecall_m, l_access_fault, s_access_fault, interrupt}) begin
+                    csr_w <= csr_rw_in;
+                    csr_waddr <= csr_rw_addr_in;
+                    csr_wdata <= csr_w_imm_mux ? {27'b0, csr_w_data_imm} : csr_w_data_reg;
+                    csr_wsc <= csr_wsc_mode_in;
+                    next_state <= STATE_MEPC;
+                end
+                else if(mret)begin
+                    csr_w <= 1;
+                    csr_waddr <= MSTATUS;
+                    csr_wdata <= mret ? {mstatus[31:8], 1'b0, mstatus[6:4], mstatus[7], mstatus[2:0]} :
+                                      {mstatus[31:8], mstatus[3], mstatus[6:4], 1'b0, mstatus[2:0]};
+                    next_state <= STATE_IDLE;
+                end 
+                else if(csr_rw_in)begin
+                    csr_w <= csr_rw_in;
+                    csr_waddr <= csr_rw_addr_in;
+                    csr_wdata <= csr_w_imm_mux ? {27'b0, csr_w_data_imm} : csr_w_data_reg;
+                    csr_wsc <= csr_wsc_mode_in;
+                    next_state <= STATE_IDLE;
+                end  
+                else begin
+                    csr_w = 0;
+                    next_state <= STATE_IDLE;
+                end     
+            end 
+            STATE_MEPC:begin
+                csr_w <= 1;
+                csr_waddr <= MEPC;
+                csr_wdata <= (interrupt) ? epc_next : epc_cur;
+                csr_wsc <= 2'b01;
+            end
+            STATE_MCAUSE:begin
+                 csr_w <= 1;
+                 csr_waddr <= MCAUSE;
+                 csr_wdata <= exception_cause;
+                 csr_wsc <= 2'b01;
+            end
+        endcase
+    end
+    always @(negedge clk) begin
+        if (rst) begin
+            csr_w <= 0;
+            csr_waddr <= 0;
+            csr_wdata <= 0;
+            csr_wsc <= 0;
+        end
+    end
+    // PC重定向控制
+    assign PC_redirect = (cur_state == STATE_IDLE && mret) ? mepc : (cur_state != STATE_IDLE) ? csr_r_data_out : 32'h0;
+    assign redirect_mux = (cur_state != STATE_IDLE || mret || csr_rw_in);
+
+    assign reg_FD_flush = (cur_state != STATE_IDLE || mret);
+    assign reg_DE_flush = (cur_state != STATE_IDLE || mret);
+    assign reg_EM_flush = (cur_state != STATE_IDLE || mret);
+    assign reg_MW_flush = (cur_state != STATE_IDLE);
+    assign RegWrite_cancel = (cur_state != STATE_IDLE);
 
 endmodule
--- arch_lab2/arch_lab2/code/core/HazardDetectionUnit.v
+++ arch_lab2/arch_lab2/code/core/HazardDetectionUnit.v
@@ -2,14 +2,59 @@
 
 module HazardDetectionUnit(
     input clk,
-    input Branch_ID, rs1use_ID, rs2use_ID,
-    input[1:0] hazard_optype_ID,
-    input[4:0] rd_EXE, rd_MEM, rs1_ID, rs2_ID, rs2_EXE,
-    output PC_EN_IF, reg_FD_stall, reg_FD_flush, reg_DE_flush,
+    input Branch_ID, rs1use_ID, rs2use_ID,//发生跳转以及使用了rs1和2的信号
+    input[1:0] hazard_optype_ID, //ctrlunit传入
+    input[4:0] rd_EXE, rd_MEM, rs1_ID, rs2_ID, rs2_EXE,//rd_EXE：ID_EXE rd_MEM:EX_MEM
+    output PC_EN_IF, reg_FD_EN, reg_FD_stall, reg_FD_flush,
+        reg_DE_EN, reg_DE_flush, reg_EM_EN, reg_EM_flush, reg_MW_EN,
     output forward_ctrl_ls,
     output[1:0] forward_ctrl_A, forward_ctrl_B
 );
+            //according to the diagram, design the Hazard Detection Unit
+    parameter hazard_optype_ALU = 2'd1;
+    parameter hazard_optype_LOAD = 2'd2;
+    parameter hazard_optype_STORE = 2'd3;
+    
+    reg[1:0] hazard_optype_EXE, hazard_optype_MEM;
+    always@(posedge clk) begin
+    //reg_EM_flush没有用到
+    hazard_optype_MEM <= hazard_optype_EXE ;//当reg_EM_flush为0时，reg中的指令不被flush,将hazard_opyupe传递
+    hazard_optype_EXE <= hazard_optype_ID & {2{~reg_DE_flush}};
+    end
 
-    //according to the diagram, design the Hazard Detection Unit
+    //Read after Write : 都使用了Rs1,需要将EXE阶段的rs1前递
+    wire rs1_forward_1     =  rd_EXE && rs1use_ID && rs1_ID==rd_EXE && hazard_optype_EXE == hazard_optype_ALU;
+    //EXE指令需要Load,而且下一条ID指令不为Store,这时前递够不到，要stall
+    wire rs1_forward_stall = rd_EXE && rs1use_ID && rs1_ID==rd_EXE && hazard_optype_EXE == hazard_optype_LOAD && hazard_optype_ID !=hazard_optype_STORE;
+    //需要将MEM阶段的rs1前递到ID阶段
+    wire rs1_forward_2     = rd_MEM && rs1use_ID && rs1_ID == rd_MEM && hazard_optype_MEM == hazard_optype_ALU;
+    //需要将MEM阶段的rs1前递到ID阶段，配合stall,相当于stall一个周期后hazard_optype传递到了MEM阶段
+    wire rs1_forward_3     = rd_MEM && rs1use_ID && rs1_ID == rd_MEM && hazard_optype_MEM == hazard_optype_LOAD;
 
+    //Read after Write : 都使用了Rs2,需要将EXE阶段的rs2前递
+    wire rs2_forward_1     = rd_EXE && rs2use_ID && rs2_ID==rd_EXE && hazard_optype_EXE == hazard_optype_ALU;
+    //EXE指令需要Load,而且下一条ID指令不为Store,这时前递够不到，要stall
+    wire rs2_forward_stall = rd_EXE && rs2use_ID && rs2_ID==rd_EXE && hazard_optype_EXE == hazard_optype_LOAD && hazard_optype_ID != hazard_optype_STORE;
+    //需要将MEM阶段的rs1前递到ID阶段
+    wire rs2_forward_2     = rd_MEM && rs2use_ID && rs2_ID == rd_MEM && hazard_optype_MEM == hazard_optype_ALU;
+    //需要将MEM阶段的rs1前递到ID阶段，配合stall,相当于stall一个周期后hazard_optype传递到了MEM阶段
+    wire rs2_forward_3     = rd_MEM && rs2use_ID && rs2_ID == rd_MEM && hazard_optype_MEM == hazard_optype_LOAD;
+    //rs1的前递
+    assign forward_ctrl_A = {2{rs1_forward_1}} & 2'd1 | {2{rs1_forward_2}} & 2'd2 | {2{rs1_forward_3}} & 2'd3 ;
+    assign forward_ctrl_B = {2{rs2_forward_1}} & 2'd1 | {2{rs2_forward_2}} & 2'd2 | {2{rs2_forward_3}} & 2'd3 ;
+    //Load+Store的情况，不需要用到ID阶段的东西，可以直接从MEM阶段判断，该信号为1时需要前递
+    assign forward_ctrl_ls = rs2_EXE == rd_MEM &&
+     hazard_optype_MEM == hazard_optype_LOAD && hazard_optype_EXE == hazard_optype_STORE;
+    //需要使流水线stall 
+    assign reg_FD_stall = rs1_forward_stall | rs2_forward_stall ;
+    //当stall发生使需要将下一条指令清除，当stall结束后重新取值
+    assign reg_DE_flush = rs1_forward_stall | rs2_forward_stall ;
+    //发生跳转时需要将进入流水线的指令flush
+    assign reg_FD_flush = Branch_ID;
+    //PC使能在没有停顿时为1，可以被更新
+    assign PC_EN_IF = ~(rs1_forward_stall | rs2_forward_stall) ;
+    assign reg_FD_EN = 1;
+    assign reg_DE_EN = 1;
+    assign reg_EM_EN = 1;
+    assign reg_MW_EN = 1;
 endmodule
